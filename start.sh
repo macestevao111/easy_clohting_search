b8d8da2f8678e8e7f4319d113ae077c39c37e902
macestevao111 <estevaolima@aol.com>
2025-03-02 23:41:15 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Ativa o ambiente virtual
+source venv/bin/activate
+# Instala Flask no ambiente virtual
+pip install Flask
 export FLASK_APP=app.py
 export FLASK_ENV=development
 flask run --host=0.0.0.0 --port=3000
